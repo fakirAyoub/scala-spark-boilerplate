0fac51b33acb6e7a078e851d388eafbe56e388f8
Ayoub Fakir <fakir.ayoub.info@gmail.com>
2019-01-02 00:15:05 +0100

    Sample Program
    The main class can be run without additional dependencies or changes.

---
+++ src/main/scala/com/fakir/samples/SampleProgram.scala
@@ -0,0 +1,27 @@
+package com.formation.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+import org.apache.spark.sql.SparkSession
+object SampleProgram {
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+
+
+    val sparkConf = new SparkConf().setAppName("My First Program in Spark!")
+      .setMaster("local[2]")
+    val sparkContext = new SparkContext(sparkConf)
+
+    val rddFromList = sparkContext.parallelize(Seq("this", "is", "an", "rdd", "from", "a", "Sequence"))
+    rddFromList.map(_.toUpperCase).foreach(println)
+
+    
+    //If you want to start working with Spark 2, go through a SparkSession!
+    val sparkSession = SparkSession.builder().getOrCreate()
+    
+    
+  }
+}
+
