ddad5b44548f0b65e11291690a436c52b1961576
fakirAyoub <ayoub@fakir.dev>
2020-10-12 12:30:17 +0200

    better runtime

--- build.sbt
+++ build.sbt
@@ -5,12 +5,6 @@ version := "0.1"
 
 scalaVersion := "2.11.11"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
-libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.2.0"
-libraryDependencies += "org.apache.tika" % "tika-core" % "1.24.1"
-libraryDependencies += "org.apache.tika" % "tika-parsers" % "1.24.1"
-
 assemblyMergeStrategy in assembly := {
   case PathList("META-INF", xs @ _*) => MergeStrategy.discard
   case x => MergeStrategy.first
--- src/main/scala/com/fakir/samples/SampleProgram.scala
+++ src/main/scala/com/fakir/samples/SampleProgram.scala
@@ -1,42 +1,8 @@
 package com.fakir.samples
-
-import java.io._
-
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
-import org.apache.tika.metadata._
-import org.apache.tika.parser._
-import org.apache.tika.parser.pdf._
 object SampleProgram {
 
   def main(args: Array[String]): Unit = {
-    Logger.getLogger("org").setLevel(Level.OFF)
-    val sparkSession = SparkSession.builder().master("local").getOrCreate()
 
-    import java.io.FileInputStream
-
-    import org.apache.tika.sax.BodyContentHandler
-    val stringWriter = new StringWriter()
-    val handler = new BodyContentHandler(stringWriter)
-    val metadata = new Metadata()
-    val inputstream = new FileInputStream(new File("pdf1.pdf"))
-    val pcontext = new ParseContext()
-
-    //parsing the document using PDF parser
-    val pdfparser = new PDFParser()
-    pdfparser.parse(inputstream, handler, metadata, pcontext)
-
-    //getting the content of the document
-    println("Contents of the PDF :" + handler.toString)
-
-    //getting metadata of the document
-    println("Metadata of the PDF:")
-    val metadataNames = metadata.names
-
-    for (name <- metadataNames) {
-      println(name + " : " + metadata.get(name))
-    }
-    
   }
 }
 
