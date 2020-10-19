a4d307040067904441cde58309c057e56c2cea3c
fakirAyoub <ayoub@fakir.dev>
2020-10-19 15:51:34 +0200

    reset

--- build.sbt
+++ build.sbt
@@ -1,9 +1,12 @@
-
 name := "scala-spark-boilerplate"
 
 version := "0.1"
 
 scalaVersion := "2.11.11"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
+libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.2.0"
 
 assemblyMergeStrategy in assembly := {
   case PathList("META-INF", xs @ _*) => MergeStrategy.discard
--- src/main/scala/com/fakir/samples/SampleProgram.scala
+++ src/main/scala/com/fakir/samples/SampleProgram.scala
@@ -1,8 +1,27 @@
 package com.fakir.samples
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+
+
 object SampleProgram {
 
-  def main(args: Array[String]): Unit = {
+  def majuscule(s: String, filtre: String): String = {
+    if(s.contains(filtre)) s
+    else s.toUpperCase
+  }
 
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.OFF)
+
+    val sparkSession = SparkSession.builder().master("local").getOrCreate()
+
+
+    val rdd = sparkSession.sparkContext.textFile("data")
+    val majRDD = rdd.map(elem => majuscule(elem, "Juventus"))
+    val barcaRDD = majRDD.filter(elem => !elem.contains("Juventus"))
+    barcaRDD.foreach(println)
   }
 }
-
